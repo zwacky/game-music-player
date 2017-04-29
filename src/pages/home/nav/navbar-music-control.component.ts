a723b4ed57d00c4878f0448480f9358ef15d7d6f
Simon Wicki <simon.wicki@justwatch.com>
2017-04-29 17:42:37 +0200

    fix: added github project link 💅

--- src/pages/home/nav/navbar-music-control.component.ts
+++ src/pages/home/nav/navbar-music-control.component.ts
@@ -2,22 +2,25 @@ import { Component } from '@angular/core';
 
 import { Store } from '@ngrx/store';
 import { Observable } from "rxjs";
-import { PlayerState } from "../../../common/player/player.reducer";
 import { AppState } from "../../../app/app.state";
 import { PlayerActions } from "../../../common/player/player.actions";
+import { isMuted, isShuffle, isRepeat } from "../../../common/player/player.selectors";
 
 
 @Component({
 	selector: 'navbar-music-control',
 	template: `
 		<ion-buttons start end class="navbar-music-control">
-			<button ion-button icon-only (click)="toggleSetting('isRepeat')" [ngClass]="{'navbar-music-control--active': (musicControl$ | async)?.isRepeat}">
+			<a href="https://github.com/zwacky/game-music-player" target="_blank" ion-button icon-only class="navbar-music-control--active" title="Go to GitHub project">
+				<ion-icon name="logo-github"></ion-icon>
+			</a>
+			<button ion-button icon-only (click)="toggleSetting('isRepeat')" [ngClass]="{'navbar-music-control--active': (isRepeat$ | async)}">
 				<ion-icon name="repeat"></ion-icon>
 			</button>
-			<button ion-button icon-only (click)="toggleSetting('isShuffle')" [ngClass]="{'navbar-music-control--active': (musicControl$ | async)?.isShuffle}">
+			<button ion-button icon-only (click)="toggleSetting('isShuffle')" [ngClass]="{'navbar-music-control--active': (isShuffle$ | async)}">
 				<ion-icon name="shuffle"></ion-icon>
 			</button>
-			<button ion-button icon-only (click)="toggleSetting('isMuted')" [ngClass]="{'navbar-music-control--active': !(musicControl$ | async)?.isMuted}">
+			<button ion-button icon-only (click)="toggleSetting('isMuted')" [ngClass]="{'navbar-music-control--active': !(isMuted$ | async)}">
 				<ion-icon [name]="(musicControl$ | async)?.isMuted ? 'volume-off' : 'volume-up'"></ion-icon>
 			</button>
 		</ion-buttons>
@@ -25,14 +28,17 @@ import { PlayerActions } from "../../../common/player/player.actions";
 })
 export class NavbarMusicControl {
 
-	public musicControl$: Observable<PlayerState>;
-	public isMuted$: Observable<boolean>;
+	isMuted$: Observable<boolean>;
+	isShuffle$: Observable<boolean>;
+	isRepeat$: Observable<boolean>;
 
 	constructor(
 		private store: Store<AppState>,
 		private playerActions: PlayerActions
 	) {
-		this.musicControl$ = this.store.select(state => state.player);
+		this.isMuted$ = this.store.select(isMuted);
+		this.isShuffle$ = this.store.select(isShuffle);
+		this.isRepeat$ = this.store.select(isRepeat);
 	}
 
 	toggleSetting(settingKey: string) {
