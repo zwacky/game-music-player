1fe6bf60f84362125ad2408e48dff7d98f8ddb30
Simon Wicki <simon.wicki@justwatch.com>
2017-07-20 23:32:08 +0200

    fix: let the service worker serve most files from cache, but update the file in parallel too 💯

--- src/service-worker.js
+++ src/service-worker.js
@@ -3,37 +3,20 @@
  * more info on how to use sw-toolbox to custom configure your service worker.
  */
 
-
 'use strict';
 importScripts('./build/sw-toolbox.js');
 
 self.toolbox.options.cache = {
-	name: 'ionic-cache'
+	name: 'gamemusicplayer-cache'
 };
 
-const CACHE_VERSION = 6;
-
-// pre-cache our key assets
-// self.toolbox.precache(
-// 	[
-// 		'./build/main.js',
-// 		'./build/main.css',
-// 		'./build/polyfills.js',
-// 		'index.html',
-// 		'manifest.json'
-// 	]
-// );
+const CACHE_VERSION = 13;
 
 // dynamically cache any other local assets
-// self.toolbox.router.any('/*', self.toolbox.cacheFirst);
 self.toolbox.router.get('/(.*)', self.toolbox.cacheFirst, {
 	origin: /fonts\.googleapis\.com$/
 });
 self.toolbox.router.get('assets/*', self.toolbox.cacheFirst);
-self.toolbox.router.get('build/*', self.toolbox.cacheFirst);
-self.toolbox.router.get('/', self.toolbox.cacheFirst);
-self.toolbox.router.get('manifest.json', self.toolbox.cacheFirst);
-
-// for any other requests go to the network, cache,
-// and then only use that cached resource if your user goes offline
-// self.toolbox.router.default = self.toolbox.networkFirst;
+self.toolbox.router.get('build/*', self.toolbox.fastest);
+self.toolbox.router.get('/', self.toolbox.fastest);
+self.toolbox.router.get('manifest.json', self.toolbox.fastest);
