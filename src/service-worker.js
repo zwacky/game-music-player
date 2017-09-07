b966b849c6520733888b79aa3ecbe20ee1ce4530
Simon Wicki <simon.wicki@justwatch.com>
2017-09-08 00:39:16 +0200

    fix: hard coded ion-label as it gets currently dropped by the ionic app scripts 😭

--- src/service-worker.js
+++ src/service-worker.js
@@ -10,7 +10,7 @@ self.toolbox.options.cache = {
 	name: 'gamemusicplayer-cache'
 };
 
-const CACHE_VERSION = 16;
+const CACHE_VERSION = 17;
 
 // dynamically cache any other local assets
 self.toolbox.router.get('/(.*)', self.toolbox.cacheFirst, {
